74feccff12012da996ee1f4c1a65990410868dfe
ladan manp <145023655+ladansj@users.noreply.github.com>
2025-11-24 19:19:21 +0330

    Refactor game.py for improved readability

--- game.py
+++ game.py
@@ -1,18 +1,14 @@
 import random
 
-# لیست انتخاب‌ها
 choices = ["سنگ", "کاغذ", "قیچی"]
 
-# انتخاب تصادفی توسط کامپیوتر
 computer_choice = random.choice(choices)
 
-# گرفتن انتخاب بازیکن
 player_choice = input("یکی از گزینه‌های 'سنگ'، 'کاغذ' یا 'قیچی' را انتخاب کن: ")
 
-# چاپ انتخاب بازیکن و کامپیوتر
 print(f"انتخاب شما: {player_choice}")
 print(f"انتخاب کامپیوتر: {computer_choice}")
-# بررسی نتیجه بازی
+
 if player_choice == computer_choice:
     print("بازی مساوی شد!")
 elif (player_choice == "سنگ" and computer_choice == "قیچی") or \
